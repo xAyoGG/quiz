84b721b40dd18e61abb9df27afca6b1904185637
ctows <ctownse6@gmu.edu>
2016-09-16 21:37:28 -0400

    Qiuz main menu

--- MainMenu.java
+++ MainMenu.java
@@ -2,29 +2,51 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.awt.*;
 
 public class MainMenu extends JFrame implements ActionListener {
 
 	private JPanel contentPane;						// Main Window
+	private String path;							// file path
+	private File file;								// file object
+	private JPanel center;							// center panel (CENTER)
+	private JPanel bottom;							// bottom panel (BOTTOM)
 	
-	// main menu items 
+	// button panel items 
 	private JPanel buttonPanel;						// Button panel
 	private JButton btnStartQuiz;					// Start Quiz button
 	private JButton btnCreateQuiz;					// Create Quiz button
 	private JButton btnModifyQuiz;					// Modify Quiz button
+	
+	// sub menu items
+	private JPanel subMenu;							// sub-menu panel
+	private JTextField word;						// Field to hold word
+	private JTextField def;							// Field to hold definition
+	private JButton	btnWordAdd;						// Add Word button
+	private JButton btnReturn;						// Finish button to hide sub-menu
+	
+	// file panel items
+	private JPanel filePanel;						// file menu panel
 	private JButton btnFilePath;					// File path button
 	private JFileChooser fc = new JFileChooser();	// File chooser object
 	private JTextField filepath;					// file path of quiz
 	
-	//sub menu items
-	private JPanel subMenu;							// sub-menu panel
-	private JTextField wordField;					// Field to hold word
-	private JTextField definitionField;				// Field to hold definition
-	private JButton	btnWordAdd;						// Add Word button
-	private JButton btnReturn;						// Finish button to hide sub-menu
-
+	// quiz panel items --
+	private JPanel quizPanel;						// quiz interface panel
+	private JLabel question;
+	private JRadioButton radio1;					
+	private JRadioButton radio2;
+	private JRadioButton radio4;
+	private JRadioButton radio3;
+	private ButtonGroup rg;							// radio button group
+	private JButton btnNext;						// next button
+	
+	// info panel items --
+	private JPanel infoPanel;						// info panel
+	private JLabel infoLabel;						// information space
+	
 	/**
 	 * Launch the application.
 	 */
@@ -55,7 +77,7 @@ public class MainMenu extends JFrame implements ActionListener {
 		BorderLayout borders = new BorderLayout(5, 5);
 		contentPane.setLayout(borders);
 		
-		// Title Panel (NORTH)
+		// Title Panel (TOP)
 		
 		JPanel titlePanel = new JPanel();
 		JLabel titleLabel = new JLabel("Vocabulary Quiz");
@@ -64,10 +86,16 @@ public class MainMenu extends JFrame implements ActionListener {
 		titlePanel.add(titleLabel);
 		contentPane.add(titlePanel,BorderLayout.NORTH);
 		
+		// Center panel (CENTER)
+		
+		center = new JPanel();
+		center.setLayout(new CardLayout());
+		contentPane.add(center, BorderLayout.CENTER);
+		
 		// Button Panel (CENTER)
 		
 		buttonPanel = new JPanel();
-		contentPane.add(buttonPanel, BorderLayout.CENTER);
+		center.add(buttonPanel);
 		buttonPanel.setLayout(null);
 		
 		btnStartQuiz = new JButton("Start Quiz");
@@ -85,33 +113,33 @@ public class MainMenu extends JFrame implements ActionListener {
 		buttonPanel.add(btnModifyQuiz);
 		btnModifyQuiz.addActionListener(this);
 		
-		// (HIDDEN) Sub-menu for create / modify quiz
+		// Sub-menu for create / modify quiz (CENTER)
 		
 		subMenu = new JPanel();
-		subMenu.setVisible(false);			// Visible = false
+		subMenu.setVisible(false);
 		subMenu.setBounds(0, 0, 446, 195);
-		buttonPanel.add(subMenu);
+		center.add(subMenu);
 		subMenu.setLayout(null);
 		
-		JLabel word = new JLabel("Word: ");
-		word.setHorizontalAlignment(SwingConstants.RIGHT);
-		word.setBounds(48, 33, 52, 14);
-		subMenu.add(word);
+		JLabel lblword = new JLabel("Word: ");
+		lblword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblword.setBounds(48, 33, 52, 14);
+		subMenu.add(lblword);
 		
-		wordField = new JTextField();
-		wordField.setBounds(110, 30, 208, 20);
-		subMenu.add(wordField);
-		wordField.setColumns(18);
+		word = new JTextField();
+		word.setBounds(110, 30, 208, 20);
+		subMenu.add(word);
+		word.setColumns(18);
 		
 		JLabel lblDefinition = new JLabel("Definition: ");
 		lblDefinition.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblDefinition.setBounds(28, 58, 72, 14);
 		subMenu.add(lblDefinition);
 		
-		definitionField = new JTextField();
-		definitionField.setBounds(110, 61, 208, 64);
-		subMenu.add(definitionField);
-		definitionField.setColumns(18);
+		def = new JTextField();
+		def.setBounds(110, 61, 208, 64);
+		subMenu.add(def);
+		def.setColumns(18);
 		
 		btnWordAdd = new JButton("Add word");
 		btnWordAdd.setBounds(110, 136, 101, 31);
@@ -123,10 +151,16 @@ public class MainMenu extends JFrame implements ActionListener {
 		subMenu.add(btnReturn);
 		btnReturn.addActionListener(this);
 		
-		// Create file selection menu
+		// Bottom panel
 		
-		JPanel filePanel = new JPanel();
-		contentPane.add(filePanel, BorderLayout.SOUTH);
+		bottom = new JPanel();
+		bottom.setLayout(new CardLayout());
+		contentPane.add(bottom, BorderLayout.SOUTH);
+		
+		// File selection menu (BOTTOM)
+		
+		filePanel = new JPanel();
+		bottom.add(filePanel);
 		
 			// File selection button
 		
@@ -140,44 +174,119 @@ public class MainMenu extends JFrame implements ActionListener {
 		filepath.setColumns(25);
 		filePanel.add(filepath);
 		filepath.setText(".\\quiz.txt");
+		
+		// Quiz panel (CENTER)
+		
+		quizPanel = new JPanel();
+		quizPanel.setVisible(false);
+		center.add(quizPanel);
+		quizPanel.setLayout(new GridLayout(6, 1, 0, 0));
+		
+		question = new JLabel("Question");
+		question.setHorizontalAlignment(SwingConstants.CENTER);
+		quizPanel.add(question);
+		
+		radio1 = new JRadioButton("radio1");
+		quizPanel.add(radio1);
+		radio1.addActionListener(this);
+		
+		radio2 = new JRadioButton("radio2");
+		quizPanel.add(radio2);
+		radio2.addActionListener(this);
+		
+		radio3 = new JRadioButton("radio3");
+		quizPanel.add(radio3);
+		radio3.addActionListener(this);
+		
+		radio4 = new JRadioButton("radio4");
+		quizPanel.add(radio4);
+		radio4.addActionListener(this);
+		
+		rg = new ButtonGroup();					// radio button group
+		rg.add(radio1);
+		rg.add(radio2);
+		rg.add(radio3);
+		rg.add(radio4);
+		
+		btnNext = new JButton("Next");
+		quizPanel.add(btnNext);
+		btnNext.addActionListener(this);
+		
+		// Info panel (SOUTH)
+		
+		infoPanel = new JPanel();
+		infoPanel.setVisible(false);
+		bottom.add(infoPanel);
+		
+		infoLabel = new JLabel(filepath.getText() + ":: Question: X/Y");
+		infoPanel.add(infoLabel);
+		
+		// add panels to center panel
+		
+        center.add(buttonPanel);
+        center.add(subMenu);
+        center.add(quizPanel);
+        
 	}
 	
 	// Action for File path button
 	
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == btnStartQuiz) {					// Start Quiz
-			System.out.println("Start Quiz");
+			buttonPanel.setVisible(false);
+			subMenu.setVisible(false);
+			quizPanel.setVisible(true);
+			filePanel.setVisible(false);
+			infoPanel.setVisible(true);
+			//...
 		}
 		if(e.getSource() == btnCreateQuiz || e.getSource() == btnModifyQuiz) {
-			subMenu.setVisible(true);						// sub-menu 'opened'
+			buttonPanel.setVisible(false);
+			subMenu.setVisible(true);					// sub-menu 'opened'
 			btnFilePath.setEnabled(false);
-			btnStartQuiz.setVisible(false);
-			btnCreateQuiz.setVisible(false);
-			btnModifyQuiz.setVisible(false);
 			if(e.getSource() == btnModifyQuiz) {			// Modify Quiz 
-				
+				//...
 			}
 			else {											// Create new Quiz
-				
+				//...
 			}
 		}
 		if(e.getSource() == btnFilePath) {					// Select File [works]
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("text document", "txt");
+			fc.setFileFilter(filter);
 			int returnVal = fc.showOpenDialog(this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				File file = fc.getSelectedFile();
-				String path = file.getAbsolutePath();
+				file = fc.getSelectedFile();
+				path = file.getAbsolutePath();
 				filepath.setText(path);
 			}
 		}
 		if(e.getSource() == btnWordAdd) {					// Add word
-			System.out.println("Add word " + wordField.getText() + ", definition " + definitionField.getText());
+			System.out.println("Add word " + word.getText() + ", definition " + def.getText());
+			//...
 		}
 		if(e.getSource() == btnReturn) {					// Return to main menu [works]
 			subMenu.setVisible(false);
-			btnStartQuiz.setVisible(true);
-			btnCreateQuiz.setVisible(true);
-			btnModifyQuiz.setVisible(true);
 			btnFilePath.setEnabled(true);
+			buttonPanel.setVisible(true);
+		}
+		if(e.getSource() == btnNext && (radio1.isSelected() || radio2.isSelected() || radio3.isSelected() || radio4.isSelected())) {						// Quiz Next button
+			String s = "";
+			if(radio1.isSelected()) {
+				s = radio1.getText();
+			}
+			else if(radio2.isSelected()) {
+				s = radio2.getText();
+			}
+			else if(radio3.isSelected()) {
+				s = radio3.getText();
+			}
+			else if(radio4.isSelected()) {
+				s = radio4.getText();
+			}
+			rg.clearSelection();
+			System.out.println("You chose " + s);
+			//...
 		}
 	}
 }
